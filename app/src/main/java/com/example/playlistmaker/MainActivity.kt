29f1b96d23c21fb3e23d7dbb11bb8fd4681e4134
Antonova MArgarita <rita@rita321.ru>
2024-05-05 19:59:54 +0300

    итоги 8ого спринта

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSearch = findViewById<Button>(R.id.btn_search)
+        btnSearch.setOnClickListener {
+            val displaySearch = Intent(this, SearchActivity::class.java)
+            startActivity(displaySearch)
         }
+
+        val btnMedia = findViewById<Button>(R.id.btn_media)
+
+        btnMedia.setOnClickListener {
+            val displayMedia = Intent(this, MediaActivity::class.java)
+            startActivity(displayMedia)
+        }
+        val btnSetting = findViewById<Button>(R.id.btn_setting)
+
+        btnSetting.setOnClickListener {
+            val displaySettings = Intent(this, SettingsActivity::class.java)
+            startActivity(displaySettings)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/activity_settings.kt
@@ -0,0 +1,32 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val arrowBackFromSettings = findViewById<ImageButton>(R.id.arrow_back_from_settings)
+
+        arrowBackFromSettings.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+    }
+}
